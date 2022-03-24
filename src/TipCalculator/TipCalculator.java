36b683bdcab05c0b7cb7621fe6e5487cb6195ca4
Sergey Korotkevich <cooler1003@gmail.com>
2022-03-24 14:52:38 +0300

    Resolve issue

--- src/TipCalculator/TipCalculator.java
+++ src/TipCalculator/TipCalculator.java
@@ -1,2 +1,26 @@
-package TipCalculator;public class TipCalculator {
+package TipCalculator;
+
+public class TipCalculator {
+
+    public static Integer calculateTip(double amount, String rating) {
+        double tip;
+        if (rating.equalsIgnoreCase("Terrible")) {
+            tip = 0;
+            return (int) Math.ceil(tip);
+        } else if (rating.equalsIgnoreCase("Poor")) {
+            tip = amount * 0.05;
+            return (int) Math.ceil(tip);
+        } else if (rating.equalsIgnoreCase("Good")) {
+            tip = amount * 0.1;
+            return (int) Math.ceil(tip);
+        } else if (rating.equalsIgnoreCase("Great")) {
+            tip = amount * 0.15;
+            return (int) Math.ceil(tip);
+        } else if (rating.equalsIgnoreCase("Excellent")) {
+            tip = amount * 0.2;
+            return (int) Math.ceil(tip);
+        } else {
+            return null;
+        }
+    }
 }
