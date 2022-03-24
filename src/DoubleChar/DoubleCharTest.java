058c52044720120064c726306191a0c02d7662cb
Sergey Korotkevich <cooler1003@gmail.com>
2022-03-24 14:54:20 +0300

    Kata: Double Char 8 kyu

--- src/DoubleChar/DoubleCharTest.java
+++ src/DoubleChar/DoubleCharTest.java
@@ -1,4 +1,14 @@
+package DoubleChar;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class DoubleCharTest {
-  
+    @Test
+    void sampleTest() {
+        assertEquals("SSttrriinngg",DoubleChar.doubleChar("String"));
+        assertEquals("HHeelllloo  WWoorrlldd",DoubleChar.doubleChar("Hello World"));
+        assertEquals("11223344!!__  ",DoubleChar.doubleChar("1234!_ "));
+    }
 }
